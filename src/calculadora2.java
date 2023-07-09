b4c7db113df2f7d05c4255650c7670920ca70520
vinculando_intellij <git@github.com:sandovalbrandon1027/vinculando_intellij.git>
2023-07-09 14:58:00 -0500

    Agregando Raiz cuadrada, Raiz cubica, punto y boton de salida.

--- src/calculadora2.java
+++ src/calculadora2.java
@@ -1,7 +1,8 @@
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-public class calculadora2 extends JDialog{
+
+public class calculadora2 extends JDialog {
     private JPanel cuerpo;
     private JTextPane textPane1;
     private JButton button1;
@@ -13,38 +14,38 @@ public class calculadora2 extends JDialog{
     private JButton uno;
     private JButton dos;
     private JButton borrartodo;
-    private JButton borrar;
-    private JButton button11;
+    private JButton raizCuadrada;
+    private JButton raizCubica;
     private JButton tres;
     private JButton seis;
     private JButton nueve;
-    private JButton button15;
+    private JButton punto;
     private JButton dividir;
     private JButton multiplicar;
     private JButton resta;
     private JButton suma;
     private JButton resultado;
-
+    private JButton salir; // Botón para salir
 
     private String operador;
     private double primerNumero;
     private double segundoNumero;
 
     public calculadora2() {
-            setContentPane(cuerpo);
-            setModal(true);
-            getRootPane().setDefaultButton(cero);
+        setContentPane(cuerpo);
+        setModal(true);
+        getRootPane().setDefaultButton(cero);
 
-            cero.addActionListener(new ActionListener() {
-                public void actionPerformed(ActionEvent e) {
-                    agregarNumero("0");
-                }
-            });
-            uno.addActionListener(new ActionListener() {
-                public void actionPerformed(ActionEvent e) {
-                    agregarNumero("1");
-                }
-            });
+        cero.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                agregarNumero("0");
+            }
+        });
+        uno.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                agregarNumero("1");
+            }
+        });
         dos.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 agregarNumero("2");
@@ -107,6 +108,11 @@ public class calculadora2 extends JDialog{
             }
         });
 
+        punto.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                agregarNumero(".");
+            }
+        });
 
         resultado.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
@@ -114,29 +120,44 @@ public class calculadora2 extends JDialog{
             }
         });
 
+        raizCuadrada.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                calcularRaizCuadrada();
+            }
+        });
 
+        raizCubica.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                calcularRaizCubica();
+            }
+        });
 
-            // Listener para el botón "Igual"
-            resultado.addActionListener(new ActionListener() {
-                public void actionPerformed(ActionEvent e) {
-                    calcularResultado();
-                }
-            });
+        // Listener para el botón "Igual"
+        resultado.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                calcularResultado();
+            }
+        });
 
-            // Listener para el botón "Limpiar"
-            borrartodo.addActionListener(new ActionListener() {
-                public void actionPerformed(ActionEvent e) {
-                    limpiarCalculadora();
-                }
-            });
+        // Listener para el botón "Limpiar"
+        borrartodo.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                limpiarCalculadora();
+            }
+        });
 
+        // Listener para el botón "Salir"
+        salir.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+    }
 
-        }
-
-        private void agregarNumero(String numero) {
-            String textoActual = textPane1.getText();
-            textPane1.setText(textoActual + numero);
-        }
+    private void agregarNumero(String numero) {
+        String textoActual = textPane1.getText();
+        textPane1.setText(textoActual + numero);
+    }
 
     private void calcularResultado() {
         String texto = textPane1.getText();
@@ -175,21 +196,32 @@ public class calculadora2 extends JDialog{
         textPane1.setText(String.valueOf(resultado));
     }
 
-
-    private void limpiarCalculadora() {
-            textPane1.setText("");
-            operador = "";
-            primerNumero = 0;
-            segundoNumero = 0;
-
-        }
-
-        public static void main(String[] args) {
-        calculadora2 dialog = new calculadora2();
-        dialog.setVisible(true);
-        System.exit(0);
+    private void calcularRaizCuadrada() {
+        String texto = textPane1.getText();
+        double numero = Double.parseDouble(texto);
+        double resultado = Math.sqrt(numero);
+        textPane1.setText(String.valueOf(resultado));
     }
 
+    private void calcularRaizCubica() {
+        String texto = textPane1.getText();
+        double numero = Double.parseDouble(texto);
+        double resultado = Math.cbrt(numero);
+        textPane1.setText(String.valueOf(resultado));
+    }
 
+    private void limpiarCalculadora() {
+        textPane1.setText("");
+        operador = "";
+        primerNumero = 0;
+        segundoNumero = 0;
+    }
 
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("calculadora2");
+        frame.setContentPane(new calculadora2().cuerpo);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
 }
